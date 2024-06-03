712d3f3046a4a2c9b50959b6cd6f7388f54eab33
Your Name <108307350+pjduri@users.noreply.github.com>
2024-06-03 01:33:24 -0500

    first commit

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents;
+
+public class EventsController : Controller
+{
+    private static List<string> Events { get; set; } = [];
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.events = Events;
+
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("/events/add")]
+    public IActionResult NewEvent(string eventName)
+    {
+        Events.Add(eventName);
+        return Redirect("/events");
+    }
+}
