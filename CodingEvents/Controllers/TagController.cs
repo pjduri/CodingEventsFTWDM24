2b2a846c07007e28ad731e6ae919c61fb84b5c53
Your Name <108307350+pjduri@users.noreply.github.com>
2024-06-17 11:15:12 -0500

    added tag model with controller, views, and dbcontext ref

---
+++ CodingEvents/Controllers/TagController.cs
@@ -0,0 +1,40 @@
+﻿using CodingEvents.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents;
+
+public class TagController : Controller
+{
+    private EventDbContext context;
+
+    public TagController(EventDbContext dbContext)
+    {
+        context = dbContext;
+    }
+
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        List<Tag> tags = context.Tags.ToList();
+        return View(tags);
+    }
+
+    public IActionResult Add()
+    {
+        Tag tag = new Tag();
+        return View(tag);
+    }
+
+    [HttpPost]
+    public IActionResult Add(Tag tag)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Tags.Add(tag);
+            context.SaveChanges();
+            return Redirect("/Tag/");
+        }
+
+        return View("Add", tag);
+    }
+}
--- CodingEvents/Migrations/20240617161308_TagMigration.Designer.cs
+++ CodingEvents/Migrations/20240617161308_TagMigration.Designer.cs
@@ -4,6 +4,7 @@ using CodingEvents.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodingEvents.Migrations
 {
     [DbContext(typeof(EventDbContext))]
-    partial class EventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240617161308_TagMigration")]
+    partial class TagMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
